88984876564007fd925fb493f4d984567c94c1a5
Sactgr <26900588+Nakultanta@users.noreply.github.com>
2019-11-20 16:05:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
 var cacheName = 'hellso-pwa';
 var filesToCache = [
+    '/',
+    '/index.html',
+    'css/style.css',
+    'js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
