3bf94b57ce6a24a6a3094ccc23c51e83d8cbb3c0
Sactgr <26900588+Nakultanta@users.noreply.github.com>
2020-02-20 13:57:16 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
-var cacheName = 'hellso-pwa';
-var filesToCache = [
-    './index.html',
-    './css/style.css',
-    './js/main.js'
-];
+var version = "v2.0.2";
+var swPath;
+var urlObject = new URL(location);
+var host;
+if (urlObject.searchParams.get("swPath")) {
+    swPath = urlObject.searchParams.get("swPath");
+}
+else {
+    if (urlObject.searchParams.get("version")) {
+        version = urlObject.searchParams.get("version");
+    }
+    if (urlObject.searchParams.get("swJSHost")) {
+        host = "https://" + urlObject.searchParams.get("swJSHost");
+    }
+    else {
+        host = "https://sdki.truepush.com/sdk/";
+    }
+    swPath = host + version + "/sw.js";
+}
+importScripts(swPath);
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
-});
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
-    );
-});
